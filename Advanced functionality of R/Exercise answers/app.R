884b1b76b83d4fd1d356ba2ba87b15439e697107
Rose Hart <rhart@bresmed.com>
2021-06-04 17:40:20 +0100

    Presentation materials

---
+++ Advanced functionality of R/Exercise answers/app.R
@@ -0,0 +1,12 @@
+#The app.R script is where the app is called from
+# See https://shiny.rstudio.com/gallery/ for examples of shiny app layouts and functionalties
+
+library("shiny")
+options(encoding = "UTF-8")
+
+App_location <- "./Advanced functionality of R/hesim shiny app/"
+
+runApp(appDir = file.path(getwd(),App_location),     # This looks for the ui.R, server.R and www folder within the wd. Do not rename these.
+       launch.browser = TRUE,                    # This line will open the application in the user's default browser
+       quiet = FALSE,
+       display.mode = "normal")
