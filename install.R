b51914c0e3ba9efadb47dbba508add0334eb26e9
Gianluca Baio <g.baio@ucl.ac.uk>
2025-10-31 10:15:34 +0000

    Update install.R

--- install.R
+++ install.R
@@ -1,19 +1,18 @@
 ### Install the relevant packages 
 
+options(
+  repos = c("https://giabaio.r-universe.dev", "https://cran.r-project.org"),
+  timeout = 600,
+  bspm.enable = FALSE,
+  bspm.sudo = FALSE
+)
 # Sets the paths
 .libPaths(c(Sys.getenv("R_LIBS_USER"), .libPaths()))
-# Sets the repos from which to install the packages
-options(repos="https://cran.r-project.org")
+
 # Sets the timeout duration
 options(timeout=600)
 # Install packages
-# Disable bspm so the package is installed from the r-universe.dev repo
-options(bspm.sudo = FALSE)
-options(bspm.enable = FALSE)
 install.packages("BCEA",repos=c("https://giabaio.r-universe.dev"),dependencies=TRUE)
-# Then re-enables it so the others can be installed quickil
-options(bspm.sudo = FALSE)
-options(bspm.enable = TRUE)
 install.packages("shiny")
 install.packages("shinydashboard")
 install.packages("hesim")
